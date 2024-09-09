0d1512646eec152dce3919211119ba748b0ac2ee
ToolReaz <toolreaz@pm.me>
2024-09-09 21:28:04 +0200

    Add todo

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # react-email-dsfr
+
 A set of premade React Email components matching French Design System style.
+
+## ToDo
+
+- [ ] Npm package
+- [ ] Components
+  - [ ] Button
+  - [ ] Link
+  - [ ] Typo
+  - [ ] Heading
+  - [ ] Card
+  - [ ] Footer
+  - [ ] Divider
+  - [ ] Alert
+  - [ ] Badge
+  - [ ] Notice
+  - [ ] Highlight
+  - [ ] Share
+  - [ ]
+- [ ] Templates
+- [ ] Theme provider (with Marainne font import)
+- [ ] GitHub page demo website
+- [ ] Documentation
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,33 +2,30 @@ import { Button as BaseButton, ButtonProps } from "@react-email/components";
 import * as React from "react";
 
 export type ButtonPropsType = ButtonProps & {
-    variant: "primary" | "secondary"
-}
+  variant: "primary" | "secondary";
+};
 
 export default function Button({ href }: ButtonPropsType) {
-    return (
-        <BaseButton
-            href={href}
-            style={styles.default}
-        >
-            Click me
-        </BaseButton>
-    );
+  return (
+    <BaseButton href={href} style={styles.default}>
+      Click me
+    </BaseButton>
+  );
 }
 
 const styles = {
-    default: {
-        fontFamily: "Marianne, Arial, sans-serif",
-        alignItems: "center",
-        backgroundColor: "#000091",
-        color: "#f5f5fe",
-        display: "inline-flex",
-        flexDirection: "row",
-        fontSize: "1rem",
-        fontWeight: 500,
-        lineHeight: "1.5rem",
-        minHeight: "2.5rem",
-        padding: ".5rem 1rem",
-        width: "fit-content",
-    }
-}
+  default: {
+    fontFamily: "Marianne, Arial, sans-serif",
+    alignItems: "center",
+    backgroundColor: "#000091",
+    color: "#f5f5fe",
+    display: "inline-flex",
+    flexDirection: "row",
+    fontSize: "1rem",
+    fontWeight: 500,
+    lineHeight: "1.5rem",
+    minHeight: "2.5rem",
+    padding: ".5rem 1rem",
+    width: "fit-content",
+  },
+};
